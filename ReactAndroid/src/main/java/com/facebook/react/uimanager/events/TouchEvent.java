c4b54796f654ebba2e49cd43e6f3628303241954
Nihar <nihar.dongara@copart.com>
2017-10-09 14:08:05 -0500

    Update TouchEvent.java

--- ReactAndroid/src/main/java/com/facebook/react/uimanager/events/TouchEvent.java
+++ ReactAndroid/src/main/java/com/facebook/react/uimanager/events/TouchEvent.java
@@ -7,154 +7,185 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-package com.facebook.react.uimanager.events;
+package com.facebook.react.uimanager;
 
-import javax.annotation.Nullable;
-
-import android.support.v4.util.Pools;
 import android.view.MotionEvent;
+import android.view.ViewGroup;
 
+import com.facebook.common.logging.FLog;
 import com.facebook.infer.annotation.Assertions;
+import com.facebook.react.common.ReactConstants;
+import com.facebook.react.uimanager.events.EventDispatcher;
+import com.facebook.react.uimanager.events.TouchEvent;
+import com.facebook.react.uimanager.events.TouchEventCoalescingKeyHelper;
+import com.facebook.react.uimanager.events.TouchEventType;
 
 /**
- * An event representing the start, end or movement of a touch. Corresponds to a single
- * {@link android.view.MotionEvent}.
- *
- * TouchEvent coalescing can happen for move events if two move events have the same target view and
- * coalescing key. See {@link TouchEventCoalescingKeyHelper} for more information about how these
- * coalescing keys are determined.
+ * JSTouchDispatcher handles dispatching touches to JS from RootViews.  If you implement RootView
+ * you need to call handleTouchEvent from onTouchEvent and onInterceptTouchEvent.  It will correctly
+ * find the right view to handle the touch and also dispatch the appropriate event to JS
  */
-public class TouchEvent extends Event<TouchEvent> {
+public class JSTouchDispatcher {
 
-  private static final int TOUCH_EVENTS_POOL_SIZE = 3;
+  private int mTargetTag = -1;
+  private final float[] mTargetCoordinates = new float[2];
+  private boolean mChildIsHandlingNativeGesture = false;
+  private long mGestureStartTime = TouchEvent.UNSET;
+  private final ViewGroup mRootViewGroup;
+  private final TouchEventCoalescingKeyHelper mTouchEventCoalescingKeyHelper =
+    new TouchEventCoalescingKeyHelper();
 
-  private static final Pools.SynchronizedPool<TouchEvent> EVENTS_POOL =
-      new Pools.SynchronizedPool<>(TOUCH_EVENTS_POOL_SIZE);
+  public JSTouchDispatcher(ViewGroup viewGroup) {
+    mRootViewGroup = viewGroup;
+  }
 
-  public static TouchEvent obtain(
-      int viewTag,
-      TouchEventType touchEventType,
-      MotionEvent motionEventToCopy,
-      float viewX,
-      float viewY,
-      TouchEventCoalescingKeyHelper touchEventCoalescingKeyHelper) {
-    TouchEvent event = EVENTS_POOL.acquire();
-    if (event == null) {
-      event = new TouchEvent();
+  public void onChildStartedNativeGesture(MotionEvent androidEvent, EventDispatcher eventDispatcher) {
+    if (mChildIsHandlingNativeGesture) {
+      // This means we previously had another child start handling this native gesture and now a
+      // different native parent of that child has decided to intercept the touch stream and handle
+      // the gesture itself. Example where this can happen: HorizontalScrollView in a ScrollView.
+      return;
     }
-    event.init(
-      viewTag,
-      touchEventType,
-      motionEventToCopy,
-      viewX,
-      viewY,
-      touchEventCoalescingKeyHelper);
-    return event;
+
+    dispatchCancelEvent(androidEvent, eventDispatcher);
+    mChildIsHandlingNativeGesture = true;
+    mTargetTag = -1;
   }
 
-  private @Nullable MotionEvent mMotionEvent;
-  private @Nullable TouchEventType mTouchEventType;
-  private short mCoalescingKey;
+  /**
+   * Main catalyst view is responsible for collecting and sending touch events to JS. This method
+   * reacts for an incoming android native touch events ({@link MotionEvent}) and calls into
+   * {@link com.facebook.react.uimanager.events.EventDispatcher} when appropriate.
+   * It uses {@link com.facebook.react.uimanager.TouchTargetHelper#findTouchTargetView}
+   * helper method for figuring out a react view ID in the case of ACTION_DOWN
+   * event (when the gesture starts).
+   */
+  public void handleTouchEvent(MotionEvent ev, EventDispatcher eventDispatcher) {
+    int action = ev.getAction() & MotionEvent.ACTION_MASK;
+    if (action == MotionEvent.ACTION_DOWN) {
+      if (mTargetTag != -1) {
+        FLog.e(
+          ReactConstants.TAG,
+          "Got DOWN touch before receiving UP or CANCEL from last gesture");
+      }
 
-  // Coordinates in the ViewTag coordinate space
-  private float mViewX;
-  private float mViewY;
-
-  private TouchEvent() {
-  }
-
-  private void init(
-      int viewTag,
-      TouchEventType touchEventType,
-      MotionEvent motionEventToCopy,
-      float viewX,
-      float viewY,
-      TouchEventCoalescingKeyHelper touchEventCoalescingKeyHelper) {
-    super.init(viewTag);
-
-    short coalescingKey = 0;
-    int action = (motionEventToCopy.getAction() & MotionEvent.ACTION_MASK);
-    switch (action) {
-      case MotionEvent.ACTION_DOWN:
-        touchEventCoalescingKeyHelper.addCoalescingKey(motionEventToCopy.getDownTime());
-        break;
-      case MotionEvent.ACTION_UP:
-        touchEventCoalescingKeyHelper.removeCoalescingKey(motionEventToCopy.getDownTime());
-        break;
-      case MotionEvent.ACTION_POINTER_DOWN:
-      case MotionEvent.ACTION_POINTER_UP:
-        touchEventCoalescingKeyHelper.incrementCoalescingKey(motionEventToCopy.getDownTime());
-        break;
-      case MotionEvent.ACTION_MOVE:
-        coalescingKey =
-          touchEventCoalescingKeyHelper.getCoalescingKey(motionEventToCopy.getDownTime());
-        break;
-      case MotionEvent.ACTION_CANCEL:
-        touchEventCoalescingKeyHelper.removeCoalescingKey(motionEventToCopy.getDownTime());
-        break;
-      default:
-        throw new RuntimeException("Unhandled MotionEvent action: " + action);
+      // First event for this gesture. We expect tag to be set to -1, and we use helper method
+      // {@link #findTargetTagForTouch} to find react view ID that will be responsible for handling
+      // this gesture
+      mChildIsHandlingNativeGesture = false;
+      mGestureStartTime = ev.getEventTime();
+      mTargetTag = TouchTargetHelper.findTargetTagAndCoordinatesForTouch(
+        ev.getX(),
+        ev.getY(),
+        mRootViewGroup,
+        mTargetCoordinates,
+        null);
+      eventDispatcher.dispatchEvent(
+        TouchEvent.obtain(
+          mTargetTag,
+          TouchEventType.START,
+          ev,
+          mGestureStartTime,
+          mTargetCoordinates[0],
+          mTargetCoordinates[1],
+          mTouchEventCoalescingKeyHelper));
+    } else if (mChildIsHandlingNativeGesture) {
+      // If the touch was intercepted by a child, we've already sent a cancel event to JS for this
+      // gesture, so we shouldn't send any more touches related to it.
+      return;
+    } else if (mTargetTag == -1) {
+      // All the subsequent action types are expected to be called after ACTION_DOWN thus target
+      // is supposed to be set for them.
+      FLog.e(
+        ReactConstants.TAG,
+        "Unexpected state: received touch event but didn't get starting ACTION_DOWN for this " +
+          "gesture before");
+    } else if (action == MotionEvent.ACTION_UP) {
+      // End of the gesture. We reset target tag to -1 and expect no further event associated with
+      // this gesture.
+      eventDispatcher.dispatchEvent(
+        TouchEvent.obtain(
+          mTargetTag,
+          TouchEventType.END,
+          ev,
+          mGestureStartTime,
+          mTargetCoordinates[0],
+          mTargetCoordinates[1],
+          mTouchEventCoalescingKeyHelper));
+      mTargetTag = -1;
+      mGestureStartTime = TouchEvent.UNSET;
+    } else if (action == MotionEvent.ACTION_MOVE) {
+      // Update pointer position for current gesture
+      eventDispatcher.dispatchEvent(
+        TouchEvent.obtain(
+          mTargetTag,
+          TouchEventType.MOVE,
+          ev,
+          mGestureStartTime,
+          mTargetCoordinates[0],
+          mTargetCoordinates[1],
+          mTouchEventCoalescingKeyHelper));
+    } else if (action == MotionEvent.ACTION_POINTER_DOWN) {
+      // New pointer goes down, this can only happen after ACTION_DOWN is sent for the first pointer
+      eventDispatcher.dispatchEvent(
+        TouchEvent.obtain(
+          mTargetTag,
+          TouchEventType.START,
+          ev,
+          mTargetCoordinates[0],
+          mTargetCoordinates[1],
+          mTouchEventCoalescingKeyHelper));
+    } else if (action == MotionEvent.ACTION_POINTER_UP) {
+      // Exactly onw of the pointers goes up
+      eventDispatcher.dispatchEvent(
+        TouchEvent.obtain(
+          mTargetTag,
+          TouchEventType.END,
+          ev,
+          mGestureStartTime,
+          mTargetCoordinates[0],
+          mTargetCoordinates[1],
+          mTouchEventCoalescingKeyHelper));
+    } else if (action == MotionEvent.ACTION_CANCEL) {
+      if (mTouchEventCoalescingKeyHelper.hasCoalescingKey(ev.getDownTime())) {
+        dispatchCancelEvent(ev, eventDispatcher);
+      } else {
+        FLog.e(
+          ReactConstants.TAG,
+          "Received an ACTION_CANCEL touch event for which we have no corresponding ACTION_DOWN"
+        );
+      }
+      mTargetTag = -1;
+    } else {
+      FLog.w(
+        ReactConstants.TAG,
+        "Warning : touch event was ignored. Action=" + action + " Target=" + mTargetTag);
     }
-    mTouchEventType = touchEventType;
-    mMotionEvent = MotionEvent.obtain(motionEventToCopy);
-    mCoalescingKey = coalescingKey;
-    mViewX = viewX;
-    mViewY = viewY;
   }
 
-  @Override
-  public void onDispose() {
-    Assertions.assertNotNull(mMotionEvent).recycle();
-    mMotionEvent = null;
-    EVENTS_POOL.release(this);
-  }
-
-  @Override
-  public String getEventName() {
-    return Assertions.assertNotNull(mTouchEventType).getJSEventName();
-  }
-
-  @Override
-  public boolean canCoalesce() {
-    // We can coalesce move events but not start/end events. Coalescing move events should probably
-    // append historical move data like MotionEvent batching does. This is left as an exercise for
-    // the reader.
-    switch (Assertions.assertNotNull(mTouchEventType)) {
-      case START:
-      case END:
-      case CANCEL:
-        return false;
-      case MOVE:
-        return true;
-      default:
-        throw new RuntimeException("Unknown touch event type: " + mTouchEventType);
+  private void dispatchCancelEvent(MotionEvent androidEvent, EventDispatcher eventDispatcher) {
+    // This means the gesture has already ended, via some other CANCEL or UP event. This is not
+    // expected to happen very often as it would mean some child View has decided to intercept the
+    // touch stream and start a native gesture only upon receiving the UP/CANCEL event.
+    if (mTargetTag == -1) {
+      FLog.w(
+        ReactConstants.TAG,
+        "Can't cancel already finished gesture. Is a child View trying to start a gesture from " +
+          "an UP/CANCEL event?");
+      return;
     }
-  }
 
-  @Override
-  public short getCoalescingKey() {
-    return mCoalescingKey;
-  }
-
-  @Override
-  public void dispatch(RCTEventEmitter rctEventEmitter) {
-    TouchesHelper.sendTouchEvent(
-        rctEventEmitter,
-        Assertions.assertNotNull(mTouchEventType),
-        getViewTag(),
-        this);
-  }
-
-  public MotionEvent getMotionEvent() {
-    Assertions.assertNotNull(mMotionEvent);
-    return mMotionEvent;
-  }
-
-  public float getViewX() {
-    return mViewX;
-  }
-
-  public float getViewY() {
-    return mViewY;
+    Assertions.assertCondition(
+      !mChildIsHandlingNativeGesture,
+      "Expected to not have already sent a cancel for this gesture");
+    Assertions.assertNotNull(eventDispatcher).dispatchEvent(
+      TouchEvent.obtain(
+        mTargetTag,
+        TouchEventType.CANCEL,
+        androidEvent,
+        mGestureStartTime,
+        mTargetCoordinates[0],
+        mTargetCoordinates[1],
+        mTouchEventCoalescingKeyHelper));
   }
 }
